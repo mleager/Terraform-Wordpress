3c712ff41aa55e39f94ba6cf03ed7f54c5a1414d
Mark <markleager92@gmail.com>
2023-09-23 00:03:57 -0400

    add terraform variables

---
+++ terraform.tfvars
@@ -0,0 +1,19 @@
+project = "Wordpress"
+
+use_amazonlinux2 = true
+
+instance_type = "t2.micro"
+
+db_identifier = "wordpress-db"
+
+db_instance_class = "db.t3.micro"
+
+db_name = "wordpress"
+
+db_user = "mark"
+
+amzn2023_user_data = "scripts/amzn2023-user-data.sh"
+
+amzn2_user_data = "scripts/amzn2-user-data.sh"
+
+amzn_user_data = "scripts/amzn-user-data.sh"
---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "project" {
+  type        = string
+  description = "Project Name"
+}
+
+variable "use_amazonlinux2" {
+  type        = bool
+  description = "Set to true if you want to use Amazon Linux 2, false for Amazon Linux 2023"
+  default     = true
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance Type for ASG controlled EC2 Instances"
+}
+
+variable "db_identifier" {
+  type        = string
+  description = "DB Identifier Name"
+}
+
+variable "db_engine_version" {
+  type        = map(list(string))
+  description = "Map of lists that holds a DB engine and its version"
+  default = {
+    "mysql"    = ["mysql", "8.0.33"]
+    "mariadb"  = ["mariadb", "10.6.14"]
+    "postgres" = ["postgres", "15.3"]
+  }
+}
+
+variable "db_instance_class" {
+  type        = string
+  description = "RDS Instance Class - default to Free Tier selection"
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  type        = string
+  description = "Name of the MySQL Database"
+}
+
+variable "db_user" {
+  type        = string
+  description = "Name of the MySQL Database User"
+}
+
+variable "amzn2023_user_data" {
+  type        = string
+  description = "Relative Path to the User Data Bash Script for Amazon Linux 2023"
+}
+
+variable "amzn2_user_data" {
+  type        = string
+  description = "Relative Path to the User Data Bash Script for Amazon Linux 2"
+}
+
+variable "amzn_user_data" {
+  type        = string
+  description = "Relative Path to the User Data Bash Script for Amazon Linux"
+}
