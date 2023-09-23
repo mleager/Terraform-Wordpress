fd97167cbf033e5102e9f04b0998eace96351501
Mark <markleager92@gmail.com>
2023-09-23 13:56:58 -0400

    add HTTPS Listener for the ALB

--- 2-alb.tf
+++ 2-alb.tf
@@ -54,14 +54,14 @@ module "alb" {
     }
   ]
 
-  # https_listeners = [
-  #   {
-  #     port               = 443
-  #     protocol           = "HTTPS"
-  #     certificate_arn    = ""
-  #     target_group_index = 0
-  #   }
-  # ]
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = module.acm.acm_certificate_arn
+      target_group_index = 0
+    }
+  ]
 
   http_tcp_listeners = [
     {
