e0415f357aadf8c5d58c02173820ea7a975b4843
Mark <markleager92@gmail.com>
2023-09-23 14:58:41 -0400

    remove HTTP Listener from ALB

--- 2-alb.tf
+++ 2-alb.tf
@@ -63,13 +63,13 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  # http_tcp_listeners = [
+  #   {
+  #     port               = 80
+  #     protocol           = "HTTP"
+  #     target_group_index = 0
+  #   }
+  # ]
 
   lb_tags = {
     Name = "${var.project}-ALB"
