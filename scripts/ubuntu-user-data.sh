a3ca8ffcbf0eb1e7321668cfc55eb4bd032d3077
Mark <markleager92@gmail.com>
2023-09-27 18:22:07 -0400

    update ubuntu user data script

--- scripts/ubuntu-user-data.sh
+++ scripts/ubuntu-user-data.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-MYSQL_USER=${db_username}
-MYSQL_DB_NAME=${db_name}
-MYSQL_PASSWORD=${db_password}
-MYSQL_ENDPOINT=${db_endpoint}
+#!/bin/bash -xe
 
 # install required packages
 sudo apt update -y
@@ -34,7 +30,7 @@ sudo mkdir /var/www/mark-dns.de
 sudo chown -R www-data.www-data /var/www/mark-dns.de/
 sudo chmod 755 /var/www/mark-dns.de/
 
-touch /etc/apache2/sites-available/mark-dns.de.conf
+sudo touch /etc/apache2/sites-available/mark-dns.de.conf
 
 sudo tee /etc/apache2/sites-available/mark-dns.de.conf > /dev/null <<EOF
 <VirtualHost *:80>
@@ -56,16 +52,8 @@ sudo a2enmod rewrite
 sudo systemctl reload apache2
 
 # download wordpress and configure it.
-
-cd /tmp/
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
-mv wordpress/* /var/www/mark-dns.de/
-chown -R www-data.www-data /var/www/mark-dns.de/
-
-cd /var/www/mark-dns.de
-cp wp-config-sample.php wp-config.php
-sed -i "s/database_name_here/$MYSQL_DB_NAME/g" wp-config.php
-sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-sed -i "s/localhost/$MYSQL_ENDPOINT/g" wp-config.php
+sudo cd /tmp/
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+sudo mv wordpress/* /var/www/mark-dns.de/
+sudo chown -R www-data.www-data /var/www/mark-dns.de/
